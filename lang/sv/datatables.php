4f7370b4d02b9e8489ab9b7d1e99292e2193701f
Tommie Lagerroos <tommie@lagerroos.nu>
2023-05-25 17:18:19 +0200

    Started translating to Swedish

---
+++ lang/sv/datatables.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'sEmptyTable'     => 'Ingen data tillgänglig i tabellen',
+    'sInfo'           => 'Visar _START_ till _END_ av _TOTAL_ inlägg',
+    'sInfoEmpty'      => 'Visar 0 till 0 av 0 inlägg',
+    'sInfoFiltered'   => '(filtrerat från _MAX_ totala inlägg)',
+    'sInfoPostFix'    => '',
+    'sInfoThousands'  => ',',
+    'sLengthMenu'     => '_MENU_ Inlägg per sida',
+    'sLoadingRecords' => 'Laddar...',
+    'sProcessing'     => 'Hanterar...',
+    'sSearch'         => 'Sök:',
+    'sZeroRecords'    => 'Inga matchande inlägg hittades',
+    'oPaginate'       => [
+        'sFirst'    => 'Första',
+        'sLast'     => 'Sista',
+        'sNext'     => 'Nästa',
+        'sPrevious' => 'Föregående',
+    ],
+    'oAria' => [
+        'sSortAscending'  => ': aktivera för att sortera kolumnen stigande',
+        'sSortDescending' => ': aktivera för att sortera kolumnen fallande',
+    ],
+];
